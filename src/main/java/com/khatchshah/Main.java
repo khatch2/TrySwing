4beff66554562832e0f7f479884324f712354ee5
khatch2_name <20730681+khatch2@users.noreply.github.com>
2025-05-04 16:53:08 +0200

    Update Main.java

--- src/main/java/com/khatchshah/Main.java
+++ src/main/java/com/khatchshah/Main.java
@@ -1,9 +1,17 @@
 package com.khatchshah;
 
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        Try2 myTry2 = new Try2();
-
+        SwingUtilities.invokeLater(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Try2 myTry2 = new Try2();
+                    }
+                }
+        );
     }
 }
